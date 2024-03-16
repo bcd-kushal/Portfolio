bc50a356608a4a8a1286688c20021360a1a5deb2
Kushal <am.kushal02@gmail.com>
2024-03-16 23:55:26 +0530

    Delete backend/middlewares directory

--- backend/middlewares/requestOrigin.ts
+++
@@ -1,8 +0,0 @@
-
-const requestOrigin = (req,res,next) => {
-    const FULL_URL = req.body.visiting_url
-    console.log(`----> request arrived from: ${FULL_URL}`)
-    next()
-}
-
-export default requestOrigin
