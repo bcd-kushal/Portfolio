636ff15f8452e5c5a29c3c826dffbe7f38e5ac41
Kushal <dev@kushalkumarsaha.com>
2024-05-26 23:50:09 +0530

    Add files via upload

--- frontend/src/defaults/types.ts
+++ frontend/src/defaults/types.ts
@@ -1,3 +1,14 @@
+
+
+export type SidebarDataType                 = { title:string, svg?:string, link?:string, isActive?:boolean, external?:boolean }
+export type SidebarDataLayoutType           = { top: SidebarDataType[], bottom: SidebarDataType[] }
+
+
+
+
+
+
+
 export type SVGType                         = { dimensions?:number|string, stroke?:string, strokeWidth?:number|string, fill?:string, class?:string }
 
 
