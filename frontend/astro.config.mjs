41a049d83aaf66e22fe442a088229d4b462b639a
Kushal <dev@kushalkumarsaha.com>
2024-08-02 23:58:59 +0530

    Add files via upload

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://portfolio.kushalkumarsaha.com',
-  integrations: [sitemap()],
-  prefetch: true
+  integrations: [tailwind()]
 });
