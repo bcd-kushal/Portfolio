f7f83782b65c810d73760ece57a072eb1135dbb5
Kushal <dev@kushalkumarsaha.com>
2024-08-02 23:58:33 +0530

    Add files via upload

---
+++ frontend/src/static/constants/socials.ts
@@ -0,0 +1,15 @@
+export const KUSHAL_MOBILE_DEFAULT = "+919123686897"
+export const KUSHAL_MOBILE_PROFESSIONAL = "+919123686897"
+export const KUSHAL_WHATSAPP = `https://wa.me/${KUSHAL_MOBILE_DEFAULT}?${encodeURIComponent("Hello, I'm coming from your portfolio and would like to have a word with you")}`
+
+export const KUSHAL_EMAIL_DEFAULT = "dev.kushal@icloud.com"
+export const KUSHAL_EMAIL_PROFESSIONAL = "dev@kushalkumarsaha.com"
+
+export const KUSHAL_GITHUB = "https://github.com/bcd-kushal"
+export const KUSHAL_LINKEDIN = "https://www.linkedin.com/in/dev-kushal/"
+
+export const KUSHAL_INSTAGRAM = "https://www.instagram.com/dev.kushalkumar/"
+export const KUSHAL_YOUTUBE = ""
+export const KUSHAL_DISCORD = ""
+export const KUSHAL_TWITTER = ""
+export const KUSHAL_DRIBBLE = ""
