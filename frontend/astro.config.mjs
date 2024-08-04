2b6767009a6714228e6a8eae53714a1896a5fb49
Kushal <dev@kushalkumarsaha.com>
2024-08-04 19:28:44 +0530

    Add files via upload

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://www.kushalkumarsaha.com",
+  integrations: [
+    tailwind(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 1.0,
+      lastmod: new Date(),
+      entryLimit: 20000,
+      customPages: [
+        "https://www.kushalkumarsaha.com/cv/CV-KushalKumarSaha.pdf",
+        "https://www.kushalkumarsaha.com/banner/kushal_banner.png",
+      ]
+    })
+  ]
 });
